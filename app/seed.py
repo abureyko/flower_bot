70fbc6d997f067d274a8a1e62a9cce90d9d3a988
Alexander Bureyko <alexander.bureyko@gmail.com>
2025-12-03 23:20:42 +0300

    fixx

--- app/seed.py
+++ app/seed.py
@@ -1,22 +1,18 @@
 # app/seed.py
 import json
-import asyncio
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, delete
 from app.db import async_session
 from app.models import Product
 
-
 async def load_products():
     async with async_session() as session:
-        # Проверяем, есть ли товары
-        result = await session.execute(select(Product))
-        existing = result.scalars().all()
+        # 1. Удаляем старые товары
+        await session.execute(delete(Product))
+        await session.commit()
 
-        #if existing:
-            #print("Товары уже есть — пропуск.")
-            #return
+        print("Все старые товары удалены.")
 
-        # читаем JSON
+        # 2. Загружаем JSON
         with open("app/static/products/products.json", "r", encoding="utf-8") as f:
             products = json.load(f)
 
@@ -31,4 +27,4 @@ async def load_products():
 
         await session.commit()
 
-    print(f"Добавлено товаров: {len(products)}")
+        print(f"Добавлено товаров: {len(products)}")
